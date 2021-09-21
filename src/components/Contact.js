890dca731efb626a6124b0c366bd8af335595b9a
Trent Smith <trentnsmith@gmail.com>
2021-09-21 13:35:31 -0500

    created aboutme page

---
+++ src/components/Contact.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <section>
+                <div class="p-5">
+                    <p class="h2">Let's Chat!</p>
+                </div>
+                <div class="p-5">
+                    <p class="h4">Call or e-mail with questions, setup a meeting, or check availability:</p>
+                    <p>Ph. 402-318-6035</p>
+                    <p>E-mail: paigekoubaphoto@gmail.com</p>
+                </div>
+                <div class="p-5">
+                    <p class="h4">Visit my social media accounts for more examples of my work and shoot me a message!</p>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Contact;
