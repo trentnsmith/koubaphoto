2824f02696598377e49bec42155360e81f864edd
Trent Smith <trentnsmith@gmail.com>
2021-09-21 13:35:53 -0500

    created pricing page

---
+++ src/components/Pricing.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+
+class Pricing extends Component {
+    render() {
+        return (
+            <section class="p-5">
+                <div class="container">
+                    <div class="row text-center">
+                        <div class="col-md pb-4">
+                            <div class="card bg-dark text-light mb-5">
+                                <div class="card-body text-left">
+                                    <h3 class="card-title mb-3">
+                                        Weddings
+                                    </h3>
+                                </div>
+                            </div>
+                            <p style={{'textAlign': 'left'}}>Lorem ipsum dolor sit amet, consectetur adipiscing 
+                            elit. Proin sed nunc tincidunt felis ultrices
+                            pretium eget eget mauris. Integer diam mi, dictum
+                            in sodales at, porttitor vitae urna. Integer 
+                            pulvinar magna leo, eget dignissim mi ultrices 
+                            vel.</p>
+                        </div>
+                        <div class="col-md pb-4">
+                            <div class="card bg-dark text-light mb-5">
+                                <div class="card-body text-left">
+                                    <h3 class="card-title mb-3">
+                                        Engagement Sessions
+                                    </h3>
+                                </div>
+                            </div>
+                            <p style={{'textAlign': 'left'}}>Lorem ipsum dolor sit amet, consectetur adipiscing 
+                            elit. Proin sed nunc tincidunt felis ultrices
+                            pretium eget eget mauris. Integer diam mi, dictum
+                            in sodales at, porttitor vitae urna. Integer 
+                            pulvinar magna leo, eget dignissim mi ultrices 
+                            vel.</p>
+                        </div>
+                        <div class="col-md pb-4">
+                            <div class="card bg-dark text-light mb-5">
+                                <div class="card-body text-left">
+                                    <h3 class="card-title mb-3">
+                                        Families/Children
+                                    </h3>
+                                </div>
+                            </div>
+                            <p style={{'textAlign': 'left'}}>Lorem ipsum dolor sit amet, consectetur adipiscing 
+                            elit. Proin sed nunc tincidunt felis ultrices
+                            pretium eget eget mauris. Integer diam mi, dictum
+                            in sodales at, porttitor vitae urna. Integer 
+                            pulvinar magna leo, eget dignissim mi ultrices 
+                            vel.</p>
+                        </div>
+                        <div class="col-md pb-4">
+                            <div class="card bg-dark text-light mb-5">
+                                <div class="card-body text-left">
+                                    <h3 class="card-title mb-3">
+                                        Lifestyle Newborn
+                                    </h3>
+                                </div>
+                            </div>
+                            <p style={{'textAlign': 'left'}}>Lorem ipsum dolor sit amet, consectetur adipiscing 
+                            elit. Proin sed nunc tincidunt felis ultrices
+                            pretium eget eget mauris. Integer diam mi, dictum
+                            in sodales at, porttitor vitae urna. Integer 
+                            pulvinar magna leo, eget dignissim mi ultrices 
+                            vel.</p>
+                        </div>
+                        <div class="col-md pb-4">
+                            <div class="card bg-dark text-light mb-5">
+                                <div class="card-body text-left">
+                                    <h3 class="card-title mb-3">
+                                        Seniors
+                                    </h3>
+                                </div>
+                            </div>
+                            <p style={{'textAlign': 'left'}}>Lorem ipsum dolor sit amet, consectetur adipiscing 
+                            elit. Proin sed nunc tincidunt felis ultrices
+                            pretium eget eget mauris. Integer diam mi, dictum
+                            in sodales at, porttitor vitae urna. Integer 
+                            pulvinar magna leo, eget dignissim mi ultrices 
+                            vel.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Pricing;
