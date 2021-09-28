7e4cac9064436bf01b9f49b246e6f40ca8be37de
Trent Smith <trentnsmith@gmail.com>
2021-09-28 13:13:16 -0500

    new component for galleries

---
+++ src/components/LifestyleGallery.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LifestyleGallery extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default LifestyleGallery;
