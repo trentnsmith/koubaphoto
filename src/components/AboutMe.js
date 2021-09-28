59872e765525feda94d0f26acf00c674fdff765c
Trent Smith <trentnsmith@gmail.com>
2021-09-28 13:12:30 -0500

    sizing img

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,8 +4,8 @@ class AboutMe extends Component {
     render() {
         return (
             <section>
-                <div>
-                   <img src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.6435-9/93032081_10158375528196468_1329102016571506688_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=IzGeYScqF_kAX80AvZ5&tn=oVCkDNk-Ei-Blm0L&_nc_ht=scontent-dfw5-2.xx&oh=2088b6bd48d93648501ebcdf3985235b&oe=616FDF63" className="img-fluid" alt="Paige"/>
+                <div class="text-center">
+                   <img style={{'width': '40%'}} src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.6435-9/93032081_10158375528196468_1329102016571506688_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=IzGeYScqF_kAX80AvZ5&tn=oVCkDNk-Ei-Blm0L&_nc_ht=scontent-dfw5-2.xx&oh=2088b6bd48d93648501ebcdf3985235b&oe=616FDF63" class="img-thumbnail rounded float-right mt-5" alt="Paige"/>
                 </div>
                 <div class="m-5">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing 
