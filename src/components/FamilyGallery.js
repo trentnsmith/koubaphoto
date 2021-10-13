7412bafe40b5f5733d1fe0088fe7abcd27a4ec14
Trent Smith <trentnsmith@gmail.com>
2021-10-13 17:27:59 -0500

    ready to add photos to carousel

--- src/components/FamilyGallery.js
+++ src/components/FamilyGallery.js
@@ -1,25 +1,117 @@
 import React, { Component } from 'react';
+import WeddingCouple1 from '../img/wedding/Wedding-Couples-1.jpg';
+import WeddingCouple2 from '../img/wedding/Wedding-Couples-2.jpg';
+import WeddingCouple3 from '../img/wedding/Wedding-Couples-3.jpg';
+import WeddingCouple4 from '../img/wedding/Wedding-Couples-4.jpg';
+import WeddingCouple5 from '../img/wedding/Wedding-Couples-5.jpg';
+import WeddingCouple6 from '../img/wedding/Wedding-Couples-6.jpg';
+import WeddingCouple7 from '../img/wedding/Wedding-Couples-7.jpg';
+import WeddingCouple8 from '../img/wedding/Wedding-Couples-8.jpg';
+import WeddingCouple9 from '../img/wedding/Wedding-Couples-9.jpg';
+import WeddingCouple10 from '../img/wedding/Wedding-Couples-10.jpg';
+import WeddingCouple11 from '../img/wedding/Wedding-Couples-11.jpg';
+import WeddingCouple12 from '../img/wedding/Wedding-Couples-12.jpg';
+import WeddingCouple13 from '../img/wedding/Wedding-Couples-13.jpg';
+import WeddingCouple14 from '../img/wedding/Wedding-Couples-14.jpg';
+import WeddingCouple15 from '../img/wedding/Wedding-Couples-15.jpg';
+import WeddingCouple16 from '../img/wedding/Wedding-Couples-16.jpg';
+import WeddingCouple17 from '../img/wedding/Wedding-Couples-17.jpg';
+import WeddingCouple18 from '../img/wedding/Wedding-Couples-18.jpg';
+import WeddingCouple19 from '../img/wedding/Wedding-Couples-19.jpg';
+import WeddingCouple20 from '../img/wedding/Wedding-Couples-20.jpg';
+import WeddingCouple21 from '../img/wedding/Wedding-Couples-21.jpg';
+import WeddingCouple22 from '../img/wedding/Wedding-Couples-22.jpg';
+import WeddingCouple23 from '../img/wedding/Wedding-Couples-23.jpg';
+import WeddingCouple24 from '../img/wedding/Wedding-Couples-24.jpg';
+import WeddingCouple25 from '../img/wedding/Wedding-Couples-25.jpg';
+
 
 class FamilyGallery extends Component {
     render() {
+
+
         return (
+            <section>
             <div class="p-3">
                 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
+                
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="https://www.hdwallpapers10.com/wp-content/uploads/2017/05/Ocean%20view%20Free%20Download.jpg" class="d-block w-100" alt="..."/>
+                        <img src={WeddingCouple1} class="d-block w-100" alt="wedding photo"/>
                     </div>
                     <div class="carousel-item">
-                        <img src="https://wallpapercave.com/wp/wp3480269.jpg" class="d-block w-100" alt="..."/>
+                        <img src={WeddingCouple2} class="d-block w-100" alt="wedding photo"/>
                     </div>
                     <div class="carousel-item">
-                        <img src="https://wallpapercave.com/wp/HUocGt4.jpg" class="d-block w-100" alt="..."/>
+                        <img src={WeddingCouple3} class="d-block w-100" alt="wedding photo"/>
                     </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple4} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple5} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple6} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple7} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple8} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple9} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple10} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple11} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple12} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple13} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple14} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple15} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple16} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple17} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple18} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple19} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple20} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple21} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple22} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple23} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple24} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={WeddingCouple25} class="d-block w-100" alt="wedding photo"/>
+                    </div>
+
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -31,6 +123,7 @@ class FamilyGallery extends Component {
                 </button>
                 </div>
             </div>
+            </section>
         );
     }
 }
