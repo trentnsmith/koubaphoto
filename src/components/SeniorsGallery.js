d8254eea23ecec8ec4d48e2b222879e874f7d56b
Trent Smith <trentnsmith@gmail.com>
2021-10-13 17:28:56 -0500

    ready for photos

--- src/components/SeniorsGallery.js
+++ src/components/SeniorsGallery.js
@@ -3,34 +3,105 @@ import React, { Component } from 'react';
 class SeniorsGallery extends Component {
     render() {
         return (
-            <div>
-               <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                    <img src="..." class="d-block w-100" alt="..." />
-                    </div>
-                    <div class="carousel-item">
-                    <img src="..." class="d-block w-100" alt="..." />
-                    </div>
-                    <div class="carousel-item">
-                    <img src="..." class="d-block w-100" alt="..." />
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-                </div> 
-            </div>
+                      // <section>
+
+          <div>
+          <div class="container">
+            <h1 class="fw-light text-center text-lg-start mt-4 mb-0">Seniors</h1>
+          </div>
+        </div>
+        // <div class="p-3">
+        //     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            
+        //     <div class="carousel-inner">
+        //         <div class="carousel-item active">
+        //             <img src={WeddingCouple1} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple2} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple3} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple4} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple5} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple6} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple7} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple8} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple9} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple10} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple11} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple12} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple13} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple14} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple15} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple16} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple17} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple18} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple19} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple20} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple21} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple22} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple23} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple24} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+        //         <div class="carousel-item">
+        //             <img src={WeddingCouple25} class="d-block w-100" alt="wedding photo"/>
+        //         </div>
+
+        //     </div>
+        //     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        //         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        //         <span class="visually-hidden">Previous</span>
+        //     </button>
+        //     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        //         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        //         <span class="visually-hidden">Next</span>
+        //     </button>
+        //     </div>
+        // </div>
+        // </section>
         );
     }
 }
