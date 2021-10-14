3f60820283c85757640f910185448917259aa1fa
Trent Smith <trentnsmith@gmail.com>
2021-10-14 17:50:39 -0500

    adding submit btn to contact form/not working yet

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,32 +4,43 @@ class Contact extends Component {
     render() {
         return (
             <div>
+                <form action="https://formsubmit.co/de9495b9afe32b18f9c580ddb019c409" method="POST">
                 <div class="row">
                     <div class="col-md-7 shadow rounded p-5">
+
+                        <input type="text" name="_honey" style={{"display": "none"}} />
+                        
+                        <input type="hidden" name="_captcha" value="false"/>
+
+                        <input type="hidden" name="_next" value="localhost:3000/success.html" />
+                        
                         <div class="row">
                             <div class="col mb-3">
-                                <label class="form-label">First Name</label>
-                                <input type="text" class="form-control" placeholder="First Name" />
+                                <label for ="firstName" class="form-label">First Name</label>
+                                <input type="text" class="form-control" name="Name" placeholder="First Name" />
                             </div>
                             <div class="col mb-3">
-                                <label class="form-label">Last  Name</label>
-                                <input type="text" class="form-control" placeholder="Last Name" />
+                                <label for="lastName" class="form-label">Last  Name</label>
+                                <input type="text" class="form-control" name="LastName" placeholder="Last Name" />
                             </div>
                         </div>
                         <div class="col mb-3">
-                                <label class="form-label">Email:</label>
-                                <input type="text" class="form-control" placeholder="name@email.com" />
+                                <label for="email" class="form-label">Email:</label>
+                                <input type="text" class="form-control" name="Email" placeholder="name@email.com" />
                             </div>
                         <div class="col mb-3">
-                            <label class="form-label">Type of session</label>
-                            <input type="text" class="form-control" placeholder="Subject" />
+                            <label for="sessionType" class="form-label">Type of session</label>
+                            <input type="text" class="form-control" name="SessionType" placeholder="Subject" />
                         </div>
                         <div class="col mb-3">
-                            <label class="form-label">Message:</label>
-                            <textarea name="" class="form-control" placeholder="Write Your Message Here" rows="3" />
-                        </div>    
+                            <label for="message" class="form-label">Message:</label>
+                            <textarea name="" class="form-control" name="Message" placeholder="Write Your Message Here" rows="3" />
+                        </div>
+                        <div class="col-md-12">
+                            <button type="submit" class="btn btn-primary form-submit-btn">Submit</button>
+                            </div>  
 
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        {/* <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                   Launch demo modal
                 </button>
                 
@@ -51,11 +62,12 @@ class Contact extends Component {
                       </div>
                     </div>
                   </div>
-                </div>
+                </div> */}
 
                     </div>
                     
                 </div>
+                </form>
                 
                 </div>
         );
