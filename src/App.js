900c12e90b6de9e56538987376b0bc716eb57850
Trent Smith <trentnsmith@gmail.com>
2021-10-22 16:55:21 -0500

    FIRST DRAFT COMPLETE/import bootstrap icons, styling complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-import MainHero from './components/MainHero';
 import MainPage from './components/MainPage';
 import Footer from './components/Footer';
 import AboutMe from './components/AboutMe';
@@ -18,8 +17,7 @@ function App() {
   return (
     <main className='App'>
       
-      {/* <Header />
-      <MainHero /> */}
+     
       <Header />
       <BrowserRouter>
         <Route exact path="/" component={MainPage}/>
